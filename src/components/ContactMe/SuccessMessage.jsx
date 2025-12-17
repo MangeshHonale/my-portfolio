4bbf5bd1827daf51e0ee3a42edee93f5bfffac78
Mangesh Honale <honalemangesh@gmail.com>
2025-12-17 11:59:26 +0530

    Fixed warnings

--- src/components/ContactMe/SuccessMessage.jsx
+++ src/components/ContactMe/SuccessMessage.jsx
@@ -2,15 +2,15 @@ import './ContactMe.css';
 
 export default function SuccessMessage() {
     return (
-        <div class="success-wrapper">
-            <div class="success-card">
-                <div class="success-icon">✔</div>
+        <div className="success-wrapper">
+            <div className="success-card">
+                <div className="success-icon">✔</div>
                 <h2>Message Sent Successfully</h2>
                 <p>
                     Thank you for reaching out. Your message has been received and I will get back to you shortly.
                 </p>
 
-                <button class="close-success-btn">Close</button>
+                <button className="close-success-btn">Close</button>
             </div>
         </div>
 
